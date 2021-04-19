a096a24e3d90372d21c9e688ad4b386370aeb921
gudanggelo <taufik_14@apps.ipb.ac.id>
2021-04-19 23:37:52 +0700

    upload method login

--- mywebapp/mywebapp/urls.py
+++ mywebapp/mywebapp/urls.py
@@ -1,12 +1,14 @@
 from django import urls
 from django.contrib import admin
-from django.urls import include, path
-from django.http import HttpResponse
+from django.urls import  path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='main-view'),
+    path('', views.index, name='home'),
     path('download', views.download, name='download'),
-    path('maps/', views.peta),
+    path('maps/', views.peta, name='maps'),
+    path('login/',views.loginPage , name='login'),
+    path('register/',views.registerPage, name='register'),
+    path('logout/', views.logoutUser, name="logout"),
 ]
--- mywebapp/mywebapp/views.py
+++ mywebapp/mywebapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.conf import settings
 from django.http import HttpResponse, Http404
 import os
@@ -6,17 +6,60 @@ import json
 import sentinelsat
 import geojson
 from sentinelsat.sentinel import SentinelAPI, read_geojson, geojson_to_wkt
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from datetime import date, datetime, timedelta
-
-
 from . import config
+from .forms import OrderForm, CreateUserForm
+from .models import *
+
+
 #method view index
 def index(request):
     return render(request,'index.html')
 
+@login_required(login_url='login')
 def peta(request):
     return render(request,'maps.html')
+
+#Creata and Login 
+def registerPage(request):
+    form =CreateUserForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user )
+            
+            return redirect('login')
+
+    context ={'form':form}
+    return render(request, 'register.html', context)
+
+def loginPage(request):
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None: 
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorret')
+
+    context = {}
+    return render(request,'login.html',context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('maps')
 
 def download(request):
     if request.method == 'POST':
