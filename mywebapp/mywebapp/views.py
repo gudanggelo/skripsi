da6bb54602e644c3a18afbcc66aa06cb0a560256
gudanggelo <taufik_14@apps.ipb.ac.id>
2021-04-28 18:04:08 +0700

    menghapus file yang tidak digunakan

--- mywebapp/mywebapp/views.py
+++ mywebapp/mywebapp/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 import json
 import sentinelsat
 import geojson
-from sentinelsat.sentinel import SentinelAPI, read_geojson, geojson_to_wkt
-from django.contrib.auth.forms import UserCreationForm
+from sentinelsat.sentinel import SentinelAPI, geojson_to_wkt
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -12,17 +11,14 @@ import os
 from datetime import date, datetime
 from . import config
 from .forms import CreateUserForm
-from .models import *
 
 
-#method view index
+
+#method access homepage
 def index(request):
     return render(request,'index.html')
-@login_required(login_url='login')
-def peta(request):
-    return render(request,'maps.html')
 
-#Creata and Login 
+#Create user accounts and redirect to login page
 def registerPage(request):
     form =CreateUserForm()
     if request.method == 'POST':
@@ -32,30 +28,34 @@ def registerPage(request):
             user = form.cleaned_data.get('username')
             messages.success(request, 'Account was created for ' + user )
             return redirect('login')
-
     context ={'form':form}
     return render(request, 'register.html', context)
 
-def loginPage(request):
-    
+#login method
+def loginPage(request):    
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
-
         user = authenticate(request, username=username, password=password)
         if user is not None: 
             login(request, user)
             return redirect('home')
         else:
             messages.info(request, 'Username OR password is incorret')
-
     context = {}
     return render(request,'login.html',context)
 
+#logout method
 def logoutUser(request):
 	logout(request)
 	return redirect('home')
+    
+#method access maps with login user 
+@login_required(login_url='login')
+def peta(request):
+    return render(request,'maps.html')
 
+#alur download
 def download(request):
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -71,7 +71,8 @@ def download(request):
                      platformname='Sentinel-1',
                      date=tanggal
                      )
-        dirpath = os.getcwd()+'/sentineldata' 
+        #menyimpan di folder sentineldata
+        dirpath = os.getcwd()+'/sentineldata'  
         for product in products:
             try:
                 api.download(product,  directory_path=dirpath, checksum=True)
@@ -82,6 +83,7 @@ def download(request):
                 os.remove(os.path.join(dirpath, item))    
         return HttpResponse(request.body)
 
+#mengatur tanggal
 def format_date(in_date):
     """Format date or datetime input or a YYYYMMDD string input to
     YYYY-MM-DDThh:mm:ssZ string format. In case you pass an
